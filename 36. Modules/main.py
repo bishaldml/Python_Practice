e775e503146f35f139b9d5c197df2892bbd034a1
Bishal <bishaldhimal321@gmail.com>
2023-02-22 16:09:04 +0545

    module

---
+++ 36. Modules/main.py
@@ -0,0 +1,13 @@
+#import message 
+#message.function1()
+#message.function2()
+
+# OR 
+#import message as msg
+#msg.function1()
+#msg.function2()
+
+# OR
+from message import function1,function2
+function1()
+function2()
---
+++ 36. Modules/message.py
@@ -0,0 +1,5 @@
+def function1():
+    print("Hello! Nice to meet you.")
+
+def function2():
+    print("Bye! See you soon.")
